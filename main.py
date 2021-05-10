b983a8da833aaee662c0df9cf9fb589fcccb9404
Prithivi Raj <ptvrajsk@gmail.com>
2021-05-10 17:05:40 +0800

    Print Formatting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+import time
+import pprint
 from src.module_parser import InputParser
 from src.module_constraints import TruckDelegator
 
@@ -10,9 +12,28 @@ if __name__ == "__main__":
     quit()
 
   try:
+    startTime = time.time()
     parser = InputParser(sys.argv[1], sys.argv[2], \
       sys.argv[3], sys.argv[4], sys.argv[5])
     truckDelegator = TruckDelegator(parser.get_parsed_data())
-    print(truckDelegator.getOptimizedRoute())
+    optimizedRoute = truckDelegator.getOptimizedRoute()
+    endTime = time.time()
+    print("\n\n")
+    print("Optimized Route:")
+    print("----")
+    for truckId, truck_load_data in optimizedRoute.items():
+      packages, route = truck_load_data
+      print(f"Trip ID:\t{truckId}")
+      print(f"Packages:\t{packages}")
+      print(f"Route:\t\t{route}")
+      print("\n")
+
+    # pprint.pprint(optimizedRoute)
+    print("\n")
+    print("Time Taken (s):")
+    print("----")
+    print(f"{endTime-startTime:.4}s")
+    print("\n\n")
+
   except ValueError as e:
     print(e.message)
